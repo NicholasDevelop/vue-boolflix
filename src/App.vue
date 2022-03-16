2ef267d82980149573104d9a76fe362e84626ff8
NicholasDevelop <marcaccini.n@gmail.com>
2022-03-16 17:13:24 +0100

    aggiunte immagini dei film

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 
   .film-container{
     display: flex;
--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-      <img src="" alt="">
+    <div class="card">
+      <img :src="`https://image.tmdb.org/t/p/w342${element.poster_path}`" alt="">
       <h3>{{ element.title }}</h3>
       <p>{{ element.original_title }}  
-        <img v-if="flags[ element.original_language ]" :src="flags[ element.original_language ]"/> 
+        <img class="flag-icon" v-if="flags[ element.original_language ]" :src="flags[ element.original_language ]"/> 
         <span v-else>{{element.original_language}}</span>
       </p>
       <p>{{ element.vote_average }}</p>
@@ -22,14 +22,22 @@ export default {
       type: Object,
     }
   },
-  // computed: {
+  // methods: {
   //   stars: function (elemnt){
-  //     return Match.floor((element.vote_average / 2))
+  //     return Math.ceil((element.vote_average / 2))
   //   }
   // }
 }
 </script>
 
 <style lang="scss">
+.card{
 
+  p{
+
+    .flag-icon{
+      width: 20px;
+    }
+  }
+}
 </style>
